75db235445e4bc27a3ea23afab636e829d83e2bd
DusicaMrksic <dusica.matic@gmail.com>
2019-05-14 23:31:09 +0200

    a

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        [Test]
+        static void Main(string[] args)
+        {
+           
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.youtube.com");
+            IWebElement element = driver.FindElements()
+        }
+    }
+}
